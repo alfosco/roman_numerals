c4218b0ac680510492332735162b441d8e32549b
Alex Fosco <alex.hf88@gmail.com>
2017-05-03 09:08:59 -0600

    starts modern romans challenge

--- test/modern_roman_numerals-test.js
+++ test/modern_roman_numerals-test.js
@@ -3,9 +3,6 @@ const expect = require('chai').expect
 
 describe('coverts numbers to new romans function', function(){
   it('returns the new roman numeral equivalent for a given integer', function(){
-    expect(convertToOldRoman(4)).to.eq("IIII")
-    expect(convertToOldRoman(83)).to.eq("LXXXIII")
-    expect(convertToOldRoman(136)).to.eq("CXXXVI")
-    expect(convertToOldRoman(2436)).to.eq("MMCCCCXXXVI")
+    
   })
 })
